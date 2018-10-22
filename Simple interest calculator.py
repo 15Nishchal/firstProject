f8c52ec1c658ac0b0fe6a058362c7818866329e9
Nishchal Gupta <44377133+15Nishchal@users.noreply.github.com>
2018-10-23 00:51:14 +0530

    added new project

---
+++ Simple interest calculator.py
@@ -0,0 +1,7 @@
+p = int(input('enter value for p'))
+r = int(input('enter value for r'))
+t = int(input('enter value for t'))
+
+si = ((p*r*t)//100)
+
+print(si)
---
+++ whether the person is adult or not.py
@@ -0,0 +1,10 @@
+age = input('enter your age')
+
+age = int(age)
+if age >=18:
+    print('you\'re an adult person')
+    print('you\'ve right to vote')
+
+else:
+    print('You\'re not an adult person')
+    print('You don\'t have right to vote')
